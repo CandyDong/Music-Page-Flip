ebac8718ca156154a5993b2fa94fbf820fcc3f30
Candy Dong <qingyid@andrew.cmu.edu>
2020-02-12 12:05:35 -0500

    added remove duplicate in midi tracker

--- Candy_Dong/midiTracker/run.py
+++ Candy_Dong/midiTracker/run.py
@@ -5,15 +5,35 @@ static_dir = "../static/"
 
 def main():
 	#clip the velocity of all notes to 127 if they are higher than that
-	path = os.path.join(static_dir, "test.mid")
+	path = os.path.join(static_dir, "VampireKillerCV1.mid")
 	f = MidiFile(path, clip=True)
+	print("midi file loaded at {}: {}\n".format(path, f))
 
-	print("midi file loaded at path {}: {}".format(path, f))
-
+	'''
+	type 0 (single track): all messages are saved in one track
+	type 1 (synchronous): all tracks start at the same time
+	type 2 (asynchronous): each track is independent of the others
+	'''
 	for track in f.tracks:
 	    print(track)
 	    for msg in track:
-	    	print(msg)
+	    	print(" ", msg)
+
+	# remove the duplicate tracks (tracks with same number of messages)
+	mes_num = set()
+	duplicates = []
+
+	for track in f.tracks:
+	    if len(track) in mes_num:
+	        duplicates.append(track)
+	    else:
+	        mes_num.add(len(track))
+
+	for track in duplicates:
+	    f.tracks.remove(track)
+
+	f.save(os.path.join(static_dir, 'VampireKillerCV1_no_dup.mid'))
+
 
 if __name__ == "__main__":
 	main()
