f0492c18a294ff23c2ce1dfb8f19d5fe48a34bfb
Candy Dong <qingyid@andrew.cmu.edu>
2020-02-13 17:18:56 -0500

    updated gitignore to not include Candy_Dong/static/; added function for randomly slice a midi file

--- Candy_Dong/midiTracker/run.py
+++ Candy_Dong/midiTracker/run.py
@@ -1,5 +1,6 @@
 import os, sys
 import mido
+import random
 
 static_dir = "../static/"
 
@@ -8,23 +9,67 @@ def getNotes(f):
 	notes = [] # each entry: (time, note) if a note_on event occurs
 	global_time = 0 # global time starts at time 0
 	for msg in f:
-		# skip meta messages
 		global_time += msg.time
 		if msg.type == "note_on":
 			notes.append((global_time, msg.note))
 	return notes
 
+# check if all messages are meta messages
+def isAllMeta(track):
+	for msg in track:
+		if not msg.is_meta:
+			return False
+	return True
+
+# create midi file from messages
+# the midi file only has one track which contains all the messages
+def createNewFile(msgs, save_path):
+	print("creating new midi file at {}".format(save_path))
+	mid = mido.MidiFile()
+	track = mido.MidiTrack()
+	mid.tracks.append(track)
+	for msg in msgs:
+		print(msg)
+		track.append(msg)
+	mid.save(save_path)
+
 # randomly slice merged_tracks and save as a new midi file 
 # for testing purpose
-def saveRandomMidiSlices(merged_tracks, save_path):
+def saveRandomMidiSlices(f, save_path, num_split=10):
+	global_time = 0
+	merged_tracks = mido.merge_tracks(f.tracks)
+	meta_track_inds = []
+	for i,msg in enumerate(merged_tracks):
+		if msg.is_meta:
+			meta_track_inds.append(i)
 
-	pass
+	num_msg = len(merged_tracks)
+	# generate population without indices of meta tracks 
+	avail_inds = list(filter(lambda i: i not in meta_track_inds, list(range(num_msg))))
+	avail_inds.sort() 
+
+	# randomly sample split points from the generated population
+	split_inds = random.sample(avail_inds, num_split)
+
+	with open(os.path.join(save_path, "split_inds.txt"), "w") as f:
+		f.write(str(split_inds))
+
+	new_msgs = []
+	for i, msg in enumerate(merged_tracks):
+		if i in split_inds:
+			if not isAllMeta(new_msgs):
+				# create a new file using the messages
+				createNewFile(new_msgs, os.path.join(save_path, "{}.mid".format(i)))
+				new_msgs = [] # reset new msgs
+				continue
+		new_msgs.append(msg)
 
 
 def main():
 	#clip the velocity of all notes to 127 if they are higher than that
 	#Chopin_-_Nocturne_Op_9_No_2_E_Flat_Major.midi
-	path = os.path.join(static_dir, "Chopin_-_Nocturne_Op_9_No_2_E_Flat_Major.midi")
+	midi_file_name = "Chopin_-_Nocturne_Op_9_No_2_E_Flat_Major"
+	path = os.path.join(static_dir, midi_file_name+".midi")
 	f = mido.MidiFile(path, clip=True)
 	print("midi file loaded at {}: {}\n".format(path, f))
 
@@ -34,40 +79,16 @@ def main():
 	type 2 (asynchronous): each track is independent of the others
 	delta time: how many ticks have passed since the last message
 	'''
-	# for track in f.tracks:
-	# 	print(track)
-	# 	for msg in track:
-	# 		print(" ", msg)
-	print("ticks per beat: {}, file type: {}".format(f.ticks_per_beat, f.type))
+	sample_save_path = os.path.join(static_dir, midi_file_name)
+	if not os.path.exists(sample_save_path):
+		os.makedirs(sample_save_path)
 
-	# print("merged tracks in playback order........................")
-	# merge tracks in playback order
-	# for msg in mido.merge_tracks(f.tracks):
-	# 	if msg.is_meta:
-	# 		print(msg)
+	saveRandomMidiSlices(f, sample_save_path)
 
-	notes = getNotes(f)
-	print(notes)
-
-	print("time length: {}".format(f.length))
-	# for msg in f:
-	# 	print(msg)
-
-
-	# # remove the duplicate tracks (tracks with same number of messages)
-	# mes_num = set()
-	# duplicates = []
-
-	# for track in f.tracks:
-	#     if len(track) in mes_num:
-	#         duplicates.append(track)
-	#     else:
-	#         mes_num.add(len(track))
-
-	# for track in duplicates:
-	#     f.tracks.remove(track)
-
-	# f.save(os.path.join(static_dir, 'VampireKillerCV1_no_dup.mid'))
+	# notes = getNotes(f)
+	# note_save_path = os.path.join(sample_save_path, "notes.txt")
+	# with open(note_save_path, "w") as f:
+	# 	f.write(str(notes))
 
 
 if __name__ == "__main__":
