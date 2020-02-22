67b8bf10e4def0d0182cc25f26e16c281fdd2b44
Althea01 <jiamengd@andrew.cmu.edu>
2020-02-22 00:45:24 -0500

    Finished getting title from input image and tracking the corresponding pdf file in database

--- Lily_Du/develop/Music_Score_Matching.py
+++ Lily_Du/develop/Music_Score_Matching.py
@@ -1,11 +1,13 @@
 import os
 from os import walk
 from wand.image import Image
-from skimage.measure import compare_ssim
-import imutils
+import PIL
+# from skimage.measure import compare_ssim
+# import imutils
 import cv2
 import numpy as np
 import pytesseract
+from fuzzywuzzy import fuzz
 
 # This function converts a pdf into an image format
 def convertPdfToImage(file, outputPath):
@@ -88,26 +90,115 @@ def mse(imageA, imageB):
     # the two images are
     return err
 
+def getTitle(inputImg):
+    image = cv2.imread(inputImg)
+    copy = image.copy()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 0, 255,
+        cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+    # find contours
+    (contours, _) = cv2.findContours(~gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) 
+
+    for contour in contours:
+        """
+        draw a rectangle around those contours on main image
+        """
+        [x,y,w,h] = cv2.boundingRect(contour)
+        cv2.rectangle(copy, (x,y), (x+w,y+h), (0, 255, 0), 1)
+    cv2.imwrite('contours.png', copy)
+
+    # create blank image of same dimension of the original image
+    mask = np.ones(copy.shape[:2], dtype="uint8") * 255 
+
+    # Collecting y value of each contour
+    (contours, _) = cv2.findContours(~gray,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) 
+
+    ys = []
+    for c in contours:
+        [x,y,w,h] = cv2.boundingRect(c)
+        ys.append(y)
+    ys.sort()
+
+    # Get the y value of possible title positions, 
+    # requires the black space above music score to be narrow
+    # threshold 20px difference between starting position of all words of a title
+    accepted_ys = []
+    for i in range(len(ys)):
+        if i == 0:
+            accepted_ys.append(ys[i])
+        else:
+            if(ys[i] - ys[i-1] <= 20):
+                accepted_ys.append(ys[i])
+            else:
+                break
+    
+    for c in contours:
+        [x,y,w,h] = cv2.boundingRect(c)
+        if y in accepted_ys:
+            cv2.drawContours(mask, [c], -1, 0, -1)
+
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, mask)
+    # load the image as a PIL/Pillow image, apply OCR, and then delete
+    # the temporary file
+    text = pytesseract.image_to_string(PIL.Image.open(filename))
+    os.remove(filename)
+    print(text)
+    # show the output images
+    cv2.imwrite("Image.png", image)
+    cv2.imwrite("Output.png", gray)
+    return text
+
+def getDBTitles(rootPath):
+    DBScoreTitles = []
+    for (dirpath, dirnames, filenames) in walk(rootPath):
+        for filename in filenames:
+            if(filename.endswith('.pdf')):
+                DBScoreTitles.append(filename.replace('.pdf', ''))
+    return DBScoreTitles
+
+
+def matchTitle(inputTitle, L):
+    maxRatio = 0
+    scoreTitle = None
+    for title in L:
+        ratio = fuzz.partial_ratio(inputTitle, title)
+        if(ratio > maxRatio):
+            maxRatio = ratio
+            scoreTitle = title
+    return scoreTitle
+
 
 def main():
     rootPath = '../static/scores'
     outputPath = '../static/images'
     inputPath = '../static/input'
 
-    # Get scores from database
-    files = getTargetFile(rootPath)
-    print('Got pdf scores from database')
+    # # Get scores from database
+    # files = getTargetFile(rootPath)
+    # print('Got pdf scores from database')
     
-    # Convert the scores from database to images and return path to that image folder
-    imagesPath = []
-    for file in files:
-        imagesPath.append(convertPdfToImage(file, outputPath))
-    print('pdf scores converted to jpeg files')
+    # # Convert the scores from database to images and return path to that image folder
+    # imagesPath = []
+    # for file in files:
+    #     imagesPath.append(convertPdfToImage(file, outputPath))
+    # print('pdf scores converted to jpeg files')
 
 
     # Get the input image
     inputImg = getInputImg(inputPath)
     print(inputImg + ' get from input folder')
+
+    # Get the title text
+    title = getTitle(inputImg)
+
+    # Get the database titles
+    dbTitles = getDBTitles(rootPath)
+
+    # Get the database title that matches the most
+    scoreTitle = matchTitle(title, dbTitles)
+    print(scoreTitle)
 
     # # Check which score in database the input matches to and return its name
     # differences = []
